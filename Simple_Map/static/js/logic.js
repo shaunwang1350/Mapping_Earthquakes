207f5e6792c0364783af933d222ab3643f90050c
Owangatang <shaunwangg@gmail.com>
2020-08-12 20:43:12 -0400

    adding points

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -4,6 +4,12 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 // let map = L.map("mapid", {center: [40.7,-94.5],zoom:4});
 
+var marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+L.circle([34.0522, -118.2437], {
+	radius: 100
+ }).addTo(map);
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -13,3 +19,4 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+

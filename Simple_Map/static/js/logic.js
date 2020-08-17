52027297f681c5411831cb665d813c9e19774782
Owangatang <shaunwangg@gmail.com>
2020-08-17 10:31:43 -0400

    GeoJSON_Linestrings

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,6 +1,6 @@
 // Add console.log to check to see if our code is working.
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 25,
 	accessToken: API_KEY
@@ -18,7 +18,7 @@ let baseMaps = {
 }
 
 let map = L.map('mapid', {
-	center: [30, 30],
+	center: [44, -80],
 	zoom: 2,
 	layers: [dark]
 })
@@ -86,9 +86,26 @@ L.polyline(line, {
 
 let airportData = "https://raw.githubusercontent.com/shaunwang1350/Mapping_Earthquakes/master/majorAirports.json";
 
+let torontoData = "https://raw.githubusercontent.com/shaunwang1350/Mapping_Earthquakes/master/torontoRoutes.json";
+
 d3.json(airportData).then(function(data){
 	console.log(data);
 	L.geoJSON(data).addTo(map);
+});
+
+let myStyle = {
+	color: "#ffffa1",
+	weight: 2
+}
+
+d3.json(torontoData).then(function(data){
+	console.log(data);
+	L.geoJSON(data, {
+		style: myStyle,
+		onEachFeature: function(feature, layer){
+			layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>")
+		}
+	}).addTo(map);
 });
 
 // Then we add our 'graymap' tile layer to the map.
